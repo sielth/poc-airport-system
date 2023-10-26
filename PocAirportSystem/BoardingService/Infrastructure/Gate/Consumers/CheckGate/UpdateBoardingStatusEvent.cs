166a9911046c008698d33a35531e1183b240e0c7
Tomas R J <49104467+TomasRJ@users.noreply.github.com>
2023-10-26 12:36:57 +0200

    Delete test code

--- PocAirportSystem/BoardingService/Infrastructure/Gate/Consumers/CheckGate/UpdateBoardingStatusEvent.cs
+++ PocAirportSystem/BoardingService/Infrastructure/Gate/Consumers/CheckGate/UpdateBoardingStatusEvent.cs
@@ -8,16 +8,3 @@ public class UpdateBoardingStatusEvent
   public required int GateNr { get; set; }
   public required GateStatus GateStatus { get; set; }
 }
-
-public class Test : IConsumer<GateUpdatedEvent>
-{
-  public Task Consume(ConsumeContext<UpdateBoardingStatusEvent> context)
-  {
-    throw new NotImplementedException();
-  }
-
-  public Task Consume(ConsumeContext<GateUpdatedEvent> context)
-  {
-    throw new NotImplementedException();
-  }
-}
