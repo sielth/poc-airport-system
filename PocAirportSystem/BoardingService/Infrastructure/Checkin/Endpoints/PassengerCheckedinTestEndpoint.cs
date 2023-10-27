699ca3e8ce8490de0450c892dc4d20351041c53a
Tomas R J <49104467+TomasRJ@users.noreply.github.com>
2023-10-27 12:40:08 +0200

    Made passenger test endpoints + small compose fix

--- PocAirportSystem/BoardingService/Infrastructure/Checkin/Endpoints/PassengerCheckedinTestEndpoint.cs
+++ PocAirportSystem/BoardingService/Infrastructure/Checkin/Endpoints/PassengerCheckedinTestEndpoint.cs
@@ -4,29 +4,31 @@ using Messages.Passenger;
 
 namespace BoardingService.Infrastructure.Checkin.Endpoints;
 
-public class PassengerCancelledTestEndpoint : Endpoint<CancelPassengerRequest>
+public class PassengerCheckedInTestEndpoint : Endpoint<CheckinPassengerRequest>
 {
   public IBus? Bus { get; set; }
   
   public override void Configure()
   {
-    Get("/api/test/CancelPassenger");
+    Get("/api/test/CheckinPassenger");
     AllowAnonymous();
   }
 
-  public override async Task HandleAsync(CancelPassengerRequest req, CancellationToken ct)
+  public override async Task HandleAsync(CheckinPassengerRequest req, CancellationToken ct)
   {
     ArgumentNullException.ThrowIfNull(Bus);
-    await Bus.Publish(new PassengerCancelledEvent{ // example of the kind of data we get from the check in group
+    await Bus.Publish(new PassengerCheckedinEvent{ // example of the kind of data we get from the check in group
       PassengerId = req.PassengerId,
       CheckinNr = req.CheckinNr,
+      FlightNr = req.FlightNr
     }, ct);
 
     await SendOkAsync(ct);
   }
 }
-public class CancelPassengerRequest
+public class CheckinPassengerRequest
 {
   public required string PassengerId { get; set; }
   public required string CheckinNr { get; set; }
+  public required string FlightNr { get; set; }
 }
